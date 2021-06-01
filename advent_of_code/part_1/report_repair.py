ba6247f50408746a1014642503ed518c1fd1a983
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-01 14:38:39 +0100

    Part 1 completed for advent of code

---
+++ advent_of_code/part_1/report_repair.py
@@ -0,0 +1,13 @@
+def report(num):
+
+    inp_file = [int(i) for i in open("part_1/inputs.txt", "r")]
+
+    for i in inp_file:
+        for j in inp_file:
+            if i + j == num:
+                return f"{i} + {j} = {num}\n{i} * {j} = {i*j}"
+
+    return "There does not seem to be a match for that input sadge :("
+
+
+print(report(2221))
