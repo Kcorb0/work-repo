1fddc409d022e5eeef162b13aa66d935d3315164
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-03 10:26:46 +0100

    Updated hangman

---
+++ advent_of_code/part_4/passport_processing.py
@@ -0,0 +1,10 @@
+# --To do--
+# Seperate each passport by empty line into a list
+# The order the fields in each passport so they can be checked
+# Could possibly use a dictionairy for this part
+# Try seperate into key value pairs
+
+# separated by spaces or newlines
+
+# Check all 8 fields are there
+# Treat cid as optional.
