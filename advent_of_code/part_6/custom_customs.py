90ea10f4d3f29abd7199a797699746bb9cad4181
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-08 00:12:53 +0100

    Advent of code part 7 planning

--- advent_of_code/part_6/custom_customs.py
+++ advent_of_code/part_6/custom_customs.py
@@ -2,9 +2,17 @@ file = "advent_of_code/part_6/input.txt"
 groups = open(file, "r").read().split("\n\n")
 individuals = [i.split("\n") for i in groups]
 
-cnt = 0
+p1_cnt = 0
+
 
 for i in individuals:
-    cnt += len(set("".join(i)))
+    p1_cnt += len(set("".join(i)))
 
-print(f"Part 1: {cnt}")
+p2_cnt = 0
+
+# cnt all letters, if greater than 1 increase count
+for i in individuals:
+    p2_cnt += len(set.intersection(*map(set, i)))
+
+print(f"Part 1: {p1_cnt}")
+print(f"Part 2: {p2_cnt}")
