a7bdc123015e45da42cf77e10012c291f4501470
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2021-06-07 23:25:47 +0100

    Advent of code part 6 complete

--- advent_of_code/part_6/custom_customs.py
+++ advent_of_code/part_6/custom_customs.py
@@ -2,4 +2,9 @@ file = "advent_of_code/part_6/input.txt"
 groups = open(file, "r").read().split("\n\n")
 individuals = [i.split("\n") for i in groups]
 
-print(individuals[0])
+cnt = 0
+
+for i in individuals:
+    cnt += len(set("".join(i)))
+
+print(f"Part 1: {cnt}")
